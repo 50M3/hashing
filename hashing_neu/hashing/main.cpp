c8598933be674f82bc29f87c1fccb09230db8f32
50M3 <36296571+50M3@users.noreply.github.com>
2019-05-13 08:15:02 +0200

    Update main.cpp

--- hashing_neu/hashing/main.cpp
+++ hashing_neu/hashing/main.cpp
@@ -8,81 +8,94 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include "CSWriter.hpp"
 
-#define MAX_HASH 8
-#define TABLE_SIZE 16
-#define FREI -1
+#define MAX_HASH 50
+#define TABLE_SIZE 100
+#define FREI 0
+#define BELEGT -1
 #define ENTFERNT -2
-#define BELEGT 1
 
-int comparisons = 0;
 
 int insert_key(int data);
 int insert_key_quad(int data);
 int search_key(int data);
+int search_key_quad(int data);
 int biggestClusterSize();
 int clusterAmount();
 void delete_key(int data);
+void delete_key_quad(int data);
 void print_hashtable();
 void print_hashtable_quad();
-void resetComparisons();
 
-int lin_hash(int data, int i);
-int quad_hash(int data, int i);
 int getHash(int data);
 
 using namespace std;
 
 
 /*
- * typedef struct key
- *  Erstellt die Struktur key
+ * typedef struct bucket
+ *  Erstellt die Struktur bucket
  *	welche aus der Data und dem State besteht
  *
  * */
-typedef struct key {
+typedef struct bucket {
 	int data;
-	int state; // -1 = frei, 0 = entfernt, 1 = belegt
-}key;
+	int state; // 0 = frei, -1 = belegt, -2 = entfernt
+}bucket;
 
-typedef struct search {
-	int erfolg = 0;
-	int erfolglos = 0;
-}search;
+/* 
+ * typedef struct anzahl
+ *  Erstellt eine Struktur anzahl
+ *  welche die erfolglosen und erfolgreichen Suchen speichert!
+ *
+ * */
+typedef struct anzahl {
+	int64_t erfolg = 0;
+	int64_t erfolglos = 0;
+}anzahl;
 
-key* hashtable[TABLE_SIZE];
-key* hashtable_quad[TABLE_SIZE];
-search anzahl;
+bucket* hashtable[TABLE_SIZE];
+bucket* hashtable_quad[TABLE_SIZE];
+anzahl zaehler;
 
 
 int main() {
 	for (int i = 0; i < TABLE_SIZE; i++) {
-		hashtable[i] = new key{ 0, FREI };
+		hashtable[i] = new bucket{ 0, FREI };
 	}
 
-	/*for (int j = 0; j < TABLE_SIZE; j++) {
-		hashtable_quad[j] = new key{ 0, FREI };
-	}*/
+	for (int i = 0; i < TABLE_SIZE; i++) {
+		hashtable_quad[i] = new  bucket{ 0, FREI };
+	}
+
+	int* randomValues;
+	randomValues = new int[MAX_HASH];
 
 	try {
 		while (true) {
-			
+
 			if (system("CLS")) system("clear");
 
 			cout << "************************************" << endl;
 			print_hashtable();
-			//print_hashtable_quad();
+			print_hashtable_quad();
 			cout << "0 -> Element in Hashtabelle einfuegen" << endl;
 			cout << "1 -> Element in Hashtabelle suchen" << endl;
 			cout << "2 -> Element in Hashtabelle QUAD einfuegen" << endl;
-			cout << "5 -> Testrutiene ausführen!" << endl;
+			cout << "4 -> Testroutine Quad ausfuehren!" << endl;
+			cout << "5 -> Testroutine ausfuehren!" << endl;
 			cout << "3 -> Programm schliessen" << endl;
 			cout << "************************************" << endl;
-			cout << "Anzahl der Schluesselvergleiche (insgesamt): " << comparisons << endl;
-			cout << "Anzahl der erfolgreichen Suchen: " << anzahl.erfolg << endl;
-			cout << "Anzahl der erfolglosen Suchen: " << anzahl.erfolglos << endl;
-			cout << "Durchschnittliche Vergleiche: " << (double)comparisons / MAX_HASH << endl;
-			cout << "Alpha: " << (double)MAX_HASH / (double)TABLE_SIZE << endl;
+			cout << "Tablesize: " << TABLE_SIZE << endl;
+			cout << "Anzahl der Schluessel " << MAX_HASH << endl;
+			cout << "Anzahl der erfolgreichen Suchen: " << zaehler.erfolg << endl;
+			cout << "Anzahl der erfolglosen Suchen: " << zaehler.erfolglos << endl;
+			cout << "Durchschnittliche Vergleiche erfolgreich: " << (double)zaehler.erfolg / MAX_HASH << endl;
+			cout << "Durchschnittliche Vergleiche erfolglos: " << (double)zaehler.erfolglos / TABLE_SIZE << endl;
+			double alpha = (double)MAX_HASH / (double)TABLE_SIZE;
+			cout << "Alpha: " << alpha << endl;
+			
 			//cout << "Lin: Anzahl der Cluster: " << clusterAmount() << endl;
 			//cout << "Lin: Groestes Cluster: " << biggestClusterSize() << endl;
 
@@ -96,36 +109,76 @@ int main() {
 			case 0:
 				cout << "-Element in Hashtabelle einfuegen" << endl;
 				cin >> eingabe;
-				resetComparisons();
 				insert_key(eingabe);
 				break;
 
 			case 1:
 				cout << "-Element in Hashtabelle suchen" << endl;
 				cin >> eingabe;
-				resetComparisons;
 				search_key(eingabe);
 				break;
 
 			case 2:
 				cout << "-Element in Hashtabelle QUAD einfuegen" << endl;
 				cin >> eingabe;
-				resetComparisons;
 				insert_key_quad(eingabe);
 				break;
 			case 3:
 				cout << "-Programm schliessen" << endl;
 				exit(EXIT_SUCCESS);
 
-			case 5:
-				cout << "-Testrutieren wird ausgeführt!" << endl;
+			case 4:
+				cout << "-Testroutine für Quad wird ausgefuehrt!" << endl;
+				
+
+				srand(time(NULL));
+
 				for (int i = 0; i < MAX_HASH; i++) {
-					insert_key(i);
-					search_key(i);
-					search_key(MAX_HASH + i);
+					randomValues[i] = (rand() << 15) + rand();
+					insert_key_quad(randomValues[i]);
 				}
-				//delete_key(4);
-				//delete_key(10);
+
+				for (int j = 0; j < MAX_HASH; j++) {
+					zaehler.erfolg += search_key_quad(randomValues[j]);
+				}
+
+				for (int j = 0; j < TABLE_SIZE; j++) {
+					zaehler.erfolglos += search_key_quad(j * -1);
+				}
+
+				/*for (int j = 0; j < MAX_HASH; j++) {
+					delete_key_quad(randomValues[j]);
+				}*/
+				break;
+
+			case 5:
+
+				srand(time(NULL));
+
+				cout << "-Testroutine wird ausgefuehrt!" << endl;
+
+				for (int i = 0; i < MAX_HASH; i++) {
+					randomValues[i] = (rand() << 15) + rand();
+					insert_key(randomValues[i]);
+					
+				}
+
+				for (int j = 0; j < MAX_HASH; j++) {
+					zaehler.erfolg += search_key(randomValues[j]);
+				}
+
+				for (int j = 0; j < TABLE_SIZE; j++) {
+					zaehler.erfolglos += search_key(j * -1);
+				}
+				
+				char file[16] = "pdf.csv";
+				CSVWriter writer(file, 1, 3);
+				writer.set(0, 0, alpha);
+				writer.set(0, 1, (double)zaehler.erfolg / MAX_HASH);
+				writer.set(0, 2, (double)zaehler.erfolglos / TABLE_SIZE);
+				writer.save();
+				cout << "Datei erfolgreich geschrieben!" << endl;
+
 				break;
 			}
 
@@ -141,33 +194,51 @@ int main() {
 /*
  * int insert_key(int data)
  *  Fuegt ein Element nach linearem Sondieren in die Liste ein!
- *	
+ *
  * Parameterliste:
  *  int data: Der Key der eingefügt werden soll.
  *
  * Rückgabeparameter:
- *  @i = Der gehashte Key
+ *  @hash = Der gehashte Key
  *
  * */
 int insert_key(int data) {
-	int i = 0, j = 0;
+	int hash = getHash(data);
 
-	do {
-		i = (lin_hash(data, j));
-		if (hashtable[i]->state == BELEGT) {
-			comparisons++;
+	// BUCKET LEER, FÜGE EIN
+	if (hashtable[hash]->state == FREI) {
+		hashtable[hash]->state = BELEGT;
+		hashtable[hash]->data = data;
+	}
+
+	else {
+
+		// SUCHE LEEREN BUCKET
+		int i = 1;
+
+		// OVERFLOW FIX
+		if (hash + i >= TABLE_SIZE)
+			i = hash * -1;
+		while (hashtable[hash + i]->state != FREI && i != 0) {
+			i++;
+
+			// OVERFLOW FIX
+			if (hash + i >= TABLE_SIZE)
+				i = hash * -1;
 		}
 
-		if (j >= TABLE_SIZE) {
+		// BUCKET IST LEER, FÜGE EIN
+		if (hashtable[hash + i]->state == FREI) {
+			hashtable[hash + i]->state = BELEGT;
+			hashtable[hash + i]->data = data;
+		}
+
+		// ALLE SLOTS SIND VOLL!
+		else {
 			throw "Hashtabelle ist voll! [insert_key]";
 		}
-		j++;
-	} while (hashtable[i]->state == BELEGT);
-
-	hashtable[i]->data = data;
-	hashtable[i]->state = BELEGT;
-
-	return i;
+	}
+		return hash;
 }
 
 /*
@@ -178,44 +249,56 @@ int insert_key(int data) {
  *  int data: Der Key der eingefügt werden soll.
  *
  * Rückgabeparameter:
- *  @i = Der gehashte Key
+ *  @hash = Der gehashte Key
  *
  * */
-/*int insert_key_quad(int data) {
-	int i = 0, j = 0;
 
-	do{
-		i = (quad_hash(data, j));
-		if (hashtable_quad[i]->state == BELEGT) {
-			comparisons++;
-		}
-		if (j > TABLE_SIZE) {
-			throw "Hashtabelle ist voll! [insert_key_quad]";
-		}
-		j++;
-	} while (hashtable_quad[i]->state == BELEGT);
-
-	hashtable_quad[i]->data = data;
-	hashtable_quad[i]->state = BELEGT;
-
-	return i;
-}*/
+// VERALTET! Muss neugeschrieben werden.
 
 int insert_key_quad(int data) {
-	int i = 1;
+	int hash = getHash(data);
 
-	do {
-		int hash = quad_hash(data, i++);
+	// SLOT FREI, FÜGE EIN
+	if (hashtable_quad[hash]->state == FREI) {
+		hashtable_quad[hash]->data = data;
+		hashtable_quad[hash]->state = BELEGT;
+	}
 
-		if (hashtable_quad[hash]->state == FREI || hashtable_quad[hash]->state == ENTFERNT) {
-			hashtable_quad[hash]->data = data;
-			hashtable_quad[hash]->state = BELEGT;
-			return hash;
+	else {
+		int i = 2;
+
+		// GENERIERE EINEN NEUEN BUCKET
+		int new_position = getHash(hash + pow(i / 2, 2) * pow(-1, i));
+
+		// ÜBERPRÜFE OB NEUER BUCKET BELEGT
+		while (hashtable_quad[new_position]->state != FREI && i < TABLE_SIZE) {
+			// NEUEN BUCKET SUCHEN
+			i++;
+			new_position = getHash(hash + pow(i / 2, 2) * pow(-1, i));
 		}
-		comparisons++;
-	} while (i < TABLE_SIZE);
+		
+		// NEUER BUCKET IST FREI, FÜGE EIN
+		if (hashtable_quad[new_position]->state == FREI) {
+			hashtable_quad[new_position]->data = data;
+			hashtable_quad[new_position]->state = BELEGT;
+		}
+		// NEUER BUCKET IST FREI, WAR ABER VORHER GEFÜLLT
+		else if(hashtable_quad[new_position]->state == ENTFERNT){
+			hashtable_quad[new_position]->data = data;
+			hashtable_quad[new_position]->state = BELEGT;
+		}
 
-	throw "Hashtabelle ist voll! [insert_key_quad]";
+		// INSERT AN EINER STELLE DIE ES NICHT GIBT
+		else if (i >= TABLE_SIZE) {
+			throw "Insert nicht in Reichweite [insert_key_quad]";
+		}
+		// HASHTABELLE IST VOLL!
+		else {
+			throw "Hashtabelle ist voll! [insert_key_quad]";
+		}
+		return hash;
+
+	}
 }
 /*
  * int search_key(int data)
@@ -225,45 +308,142 @@ int insert_key_quad(int data) {
  *  int data: Der Key der eingefügt werden soll.
  *
  * Rückgabeparameter:
- *  @i = Der gehashte Key
- *	@FREI = Ein leerer Slot wurde entdeckt!
+ *  @count = RÜCKGABE DER GEZÄHLTEN VERGLEICHE
  *
  * */
 
 int search_key(int data) {
-	int i = 0, hash = lin_hash(data, i++);
+	int i = 1, hash = getHash(data), count = 1;
 
-	while (hashtable[hash]->state == BELEGT && i < TABLE_SIZE) {
-		comparisons++;
 
-		if (hashtable[hash]->data == data) {
-			anzahl.erfolg += 1;
-			return hash;
+	// BUCKET FREI
+	if (hashtable[hash]->state == FREI) {
+		return 0;
+	}
+
+	// BUCKET INHALT = GESUCHTER DATA
+	if (hashtable[hash]->data == data) {
+		return count;
+	}
+
+	// PASSENDEN BUCKET SUCHEN
+	else {
+		count++;
+
+		// OVERFLOW FIX
+		if (hash + i >= TABLE_SIZE) {
+			i = hash * -1;
+
 		}
 
-		hash = lin_hash(data, i++);
+		// SOLANGE SUCHEN BIS BUCKET DATA = GESUCHTER DATA
+		while (hashtable[hash + i]->data != data && i != 0 && hashtable[hash + i]->state != FREI) {
+			i++;
+			count++;
+
+			// OVERFLOW FIX
+			if (hash + i >= TABLE_SIZE) {
+				i = hash * -1;
+
+			}
+		}
+		return count;
 	}
-	
-	anzahl.erfolglos += 1;
-	return FREI;
 }
 
+// MUSS NOCH GETESTET WERDEN!
+
+int search_key_quad(int data) {
+	int count = 1, hash = getHash(data), i = 1;
+
+	if (hashtable_quad[hash]->state == FREI) {
+		return 0;
+	}
+
+	if (hashtable_quad[hash]->data == data) {
+		return count;
+	}
+	else {
+		count++;
+		int new_position = getHash(hash + pow(i / 2, 2) * pow(-1, i));
+		while (hashtable_quad[new_position]->data != data && i < TABLE_SIZE && hashtable_quad[new_position]->state != FREI) {
+			i++;
+			count++;
+			new_position = getHash(hash + pow(i / 2, 2) * pow(-1, i));
+		}
+
+		if (hashtable_quad[new_position]->data == data) {
+			return count;
+		}
+		else {
+			return count;
+		}
+	}
+}
+
+
+/*
+ * void delete_key(int data)
+ *  Sucht und löscht das Element data
+ *
+ * Parameterliste:
+ *  int data: Die zu suchende und löschende Data
+ *
+ * Rückgabeparameter:
+ *  void
+ *
+ * */
 void delete_key(int data) {
-	int hash = 0, i = 0;
+	int hash = getHash(data);
 
-	do {
-		hash = (lin_hash(data, i));
-		if (hashtable[hash]->state == BELEGT) {
-			comparisons++;
-		}
-		i++;
-	} while (hashtable[hash]->state == BELEGT && hashtable[hash]->data != data);
-
-	if (hashtable[hash]->state == BELEGT && hashtable[hash]->data == data) {
-		hashtable[hash]->state = ENTFERNT;
+	// BUCKET DATA GEFUNDEN, LÖSCHE DATA, SETZE STATE
+	if (hashtable[hash]->data == data) {
 		hashtable[hash]->data = 0;
+		hashtable[hash]->state = ENTFERNT;
+	}
+
+	else {
+		// BUCKET DATA NICHT GEFUNDEN, SUCHE DATA
+		int i = 1;
+		while (hashtable[hash]->data != data && i != 0) {
+			i++;
+
+			// OVERFLOW FIX
+			if (hash + i >= TABLE_SIZE) {
+				i = hash * -1;
+			}
+		}
+
+		// DATA GEFUNDEN, LÖSCHE DATA, SETZE STATE
+		if (hashtable[hash + i]->data == data) {
+			hashtable[hash + i]->data = 0;
+			hashtable[hash + i]->state = ENTFERNT;
+		}
 	}
 }
+
+void delete_key_quad(int data) {
+	int hash = getHash(data);
+
+	if (hashtable_quad[hash]->data == data) {
+		hashtable_quad[hash]->data = 0;
+		hashtable_quad[hash]->state = ENTFERNT;
+	}
+
+	else {
+		int i = 2;
+		int new_position = getHash(hash + pow(i / 2, 2) * pow(-1, i));
+		while (hashtable_quad[new_position]->data != data && i != 0) {
+			i++;
+			new_position = getHash(hash + pow(i / 2, 2) * pow(-1, i));
+		}
+		if (hashtable_quad[new_position]->data == data) {
+			hashtable_quad[new_position]->data = 0;
+			hashtable_quad[new_position]->state = ENTFERNT;
+		}
+	}
+}
+	
 
 /*
  * int biggestClusterSize()
@@ -274,7 +454,7 @@ void delete_key(int data) {
  *  @ max = return max;
  *
  * */
-int biggestClusterSize(){
+int biggestClusterSize() {
 	int count = 0, max = 0;
 
 	for (int i = 0; i < TABLE_SIZE; i++) {
@@ -320,76 +500,21 @@ int clusterAmount() {
 
 
 /*
- * double alpha(int capacity, int number_of_elements)
- *  Gibt an zu wie viel % die Liste gefüllt ist
- * 
- * Parameterliste:
- *	int capacity: Größe der Tabelle
- *  int number_of_elements: Anzahl der Elemente in der Liste
- *
- * Rückgabeparamter:
- *  @number_of_elements * 1.0 / capacity = Füllstand der Hashtabelle
- *
- * */
-double alpha(int capacity, int number_of_elements) {
-	return number_of_elements * 1.0 / capacity;
-}
-
-double lin_success(double alpha) {
-	return 0.5* (1 + (1 / (1 - alpha)));
-}
-
-double lin_fail(double alpha) {
-	return 0.5* (1 + (1 / (1 - alpha) * (1 - alpha)));
-}
-
-
-
-/*
- * int lin_hash(int data, int i)
- *  Hash nach linearer Sondierung
- *
- * Parameterliste:
- *  int data: Der Key der eingefügt werden soll.
- *  int i: Position in der Angefangen wird Keys einzufügen!
- *
- * Rückgabeparameter:
- *  @(getHash(data) + i) % MAX_HASH = Linear gehashter Key
- *
- * */
-int lin_hash(int data, int i) {
-	return (getHash(data) + i) % MAX_HASH;
-}
-
-/*
- * int quad_hash(int data, int i)
- *  Hash nach quadratischer Sondierung
- *
- * Parameterliste:
- *  int data: Der Key der eingefügt werden soll.
- *  int i: Position in der Angefangen wird Keys einzufügen!
- *
- * Rückgabeparameter:
- *  @getHash(getHash(data) + (pow(i / 2, 2) * pow(-1, i)));H = Quadratisch sondierter Key
- *
- * */
-int quad_hash(int data, int i) {
-	return getHash(getHash(data) + (pow(i / 2, 2) * pow(-1, i)));
-}
-
-/*
  * int getHash(int data)
- *  Gehashter Key nach Divisionsmethode!
+ *  Gehashter Key mit Modulo von Wikipedia für -a mod m!
  *
  * Parameterliste:
  *  int data: Der Key der gehasht wird.
  *
  * Rückgabeparameter:
- *  @data % MAX_HASH = Gehashter Key
+ *  @calc Der gehashte Schlüssel 
  *
  * */
 int getHash(int data) {
-	return data % MAX_HASH;
+	int calc = data % TABLE_SIZE;
+	if (calc < 0)
+		calc += TABLE_SIZE;
+	return calc;
 }
 
 
@@ -397,8 +522,8 @@ void print_hashtable() {
 	cout << "------------------------------------" << endl;
 	for (int i = 0; i < TABLE_SIZE; i++) {
 		cout << setw(3) << i << " Memory " << hashtable[i] << " -> "
-			 << setw(5) << hashtable[i]->data << " -> "
-			 << hashtable[i]->state << endl;
+			<< setw(5) << hashtable[i]->data << " -> "
+			<< hashtable[i]->state << endl;
 	}
 	cout << "------------------------------------" << endl;
 }
@@ -411,8 +536,4 @@ void print_hashtable_quad() {
 			<< hashtable_quad[i]->state << endl;
 	}
 	cout << "------------------------------------" << endl;
-}
-
-void resetComparisons(){
-	comparisons = 0;
 }
